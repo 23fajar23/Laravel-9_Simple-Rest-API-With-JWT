e101756b3d9130d5e39769a63924a13c3601a7a7
23fajar23 <fajardarussalam20@gmail.com>
2024-07-03 14:46:07 +0700

    [IMPROVE] set dummy data for product and category product

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -4,6 +4,8 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
+use App\Models\Category_product;
+use App\Models\Product;
 
 class DatabaseSeeder extends Seeder
 {
@@ -14,6 +16,34 @@ class DatabaseSeeder extends Seeder
      */
     public function run()
     {
-        // \App\Models\User::factory(10)->create();
+
+        Category_product::create([
+            'name' => 'snack'
+        ]);
+
+        Category_product::create([
+            'name' => 'drink'
+        ]);
+
+        Category_product::create([
+            'name' => 'food'
+        ]);
+
+        $category_id = Category_product::first()->id;
+
+        Product::create([
+            'product_category_id' => $category_id,
+            'name' => 'Oreo',
+            'price' => 7000,
+            'image' => 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full/MTA-35563760/oreo_oreo_biscuit_133gr_pak_full02_netmv3vz.png'
+        ]);
+
+        Product::create([
+            'product_category_id' => $category_id,
+            'name' => 'Mie Gelas',
+            'price' => 14000,
+            'image' => 'https://www.static-src.com/wcsstore/Indraprastha/images/catalog/full//100/MTA-53509149/mie-gelas_mie-gelas-rencengan-isi-10-pcs-all-varian-rasa_full01.jpg'
+        ]);
+
     }
 }
